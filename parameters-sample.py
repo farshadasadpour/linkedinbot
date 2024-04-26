2187da9ff06a58eb1dd2bb957a8073cd5642d974
Farshad <asadpoor.f@gmail.com>
2024-04-26 18:53:39 +0330

    added new params and future

--- parameters-sample.py
+++ parameters-sample.py
@@ -1,5 +1,11 @@
-file_name = 'connections.csv'
 linkedin_username = 'linkedinuser'
 linkedin_password = 'linkedinpassword'
 keywords = 'DevOps,Linux,Python,AWS,SRE'
+geoUrn = ''
+start_page = 10
 till_page = 200
+google_chrome_driver_path= ''
+
+# 102890719 Netherland
+# 100565514 Belgium
+# 101282230 Germany
